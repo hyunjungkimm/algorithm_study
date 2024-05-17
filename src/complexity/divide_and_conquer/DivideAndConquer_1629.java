9fbd62dfaf60392c00718dc8920701156da9bb83
kimhyeonjeong <khjjung1124@gmail.com>
2024-05-17 21:27:43 +0900

    [Divide And Conquer Algorithm] 20240517 - 백준 1629 분할정복

---
+++ src/complexity/divide_and_conquer/DivideAndConquer_1629.java
@@ -0,0 +1,27 @@
+package src.complexity.divide_and_conquer;
+
+import java.util.Scanner;
+
+public class DivideAndConquer_1629 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+        System.out.println(multiplication(A, B, C));
+    }
+
+    static long multiplication(long A, long B, long C) {
+        if(B == 0) return 1;
+
+        long half = multiplication(A, B/2, C);
+        long result = (half * half) % C;
+
+        if(B % 2 == 1) {
+            result = (A * result) % C;
+        }
+
+        return result;
+    }
+}
