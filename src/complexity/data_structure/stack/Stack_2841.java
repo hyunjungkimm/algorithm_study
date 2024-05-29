cb1f27ac01ca104a10b23719ff1463feeb23ff37
kimhyeonjeong <khjjung1124@gmail.com>
2024-05-29 17:48:33 +0900

    [Stack] 20240528 - 백준 2841, 3986

---
+++ src/complexity/data_structure/stack/Stack_2841.java
@@ -0,0 +1,50 @@
+package src.complexity.data_structure.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Stack_2841 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int count = 0;
+        Map<Integer,Stack> map = new HashMap<>();
+
+        for(int i = 0; i < N; i++) {
+            String[] number = br.readLine().split(" ");
+            int tone = Integer.parseInt(number[0]);
+            int flat = Integer.parseInt(number[1]);
+            if(map.containsKey(tone)) {
+                Stack<Integer> stack = map.get(tone);
+                if (!stack.isEmpty()){
+                    if(stack.peek() < flat) {
+                        stack.push(flat);
+                        count++;
+                    } else if(stack.peek() > flat){
+                        while(stack.size() > 0 && stack.peek() > flat){
+                            stack.pop();
+                            count++;
+                        }
+                        if(stack.isEmpty()) {
+                            stack.push(flat);
+                            count++;
+                        }else if(stack.peek() != flat){
+                            stack.push(flat);
+                            count++;
+                        }
+                    }
+                }
+            }else {
+                Stack<Integer> stack = new Stack<>();
+                stack.push(flat);
+                count++;
+                map.put(tone,stack);
+            }
+        }
+
+        System.out.println(count);
+    }
+}
---
+++ src/complexity/data_structure/stack/Stack_3986.java
@@ -0,0 +1,27 @@
+package src.complexity.data_structure.stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Stack_3986 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for(int i = 0; i < N; i++){
+            char[] input = br.readLine().toCharArray();
+            Stack<Character> stream = new Stack<>();
+            for(int j = 0; j < input.length; j++){
+                if(stream.isEmpty() || stream.peek() != input[j]){
+                    stream.push(input[j]);
+                }else if(stream.peek() == input[j]){
+                    stream.pop();
+                }
+            }
+            if(stream.isEmpty()) count++;
+        }
+        System.out.println(count);
+    }
+}
