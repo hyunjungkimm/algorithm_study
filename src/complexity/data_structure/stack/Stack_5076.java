0781f39188488759dfb4d78f0ddbab1097e8e676
kimhyeonjeong <khjjung1124@gmail.com>
2024-05-30 19:39:29 +0900

    [Stack] 20240530 - 백준 5076, 2304 - stack으로 풀다가 계속 틀려서 stack으로 안풀고 통과

---
+++ src/complexity/data_structure/stack/Stack_5076.java
@@ -0,0 +1,62 @@
+package src.complexity.data_structure.stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Stack_5076 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+
+        while(!(input.equals("#"))) {
+            input = br.readLine();
+            if(input.equals("#")) break;
+            char[] arr = input.toCharArray();
+            StringBuffer sb = new StringBuffer();
+            Stack<String> stack = new Stack<>();
+
+            for(int i = 0; i < arr.length; i++) {
+                if(arr[i] == '<') {
+                    i++;
+
+                    if(arr[i] == '/') {
+                        i++;
+                        while(arr[i] != '>') {
+                            sb.append(arr[i]);
+                            i++;
+                        }
+                        if(!stack.empty()) {
+                            if(stack.peek().equals(sb.toString())) {
+                                stack.pop();
+                                sb.setLength(0);
+                            }
+                        }
+                    }else{
+                        while(arr[i] != '>') {
+                            if (arr[i] == '/'){
+                                sb.setLength(0);
+                                break;
+                            }
+                            if(arr[i] == ' ' && arr[i+1] !='/') break;
+                            sb.append(arr[i]);
+                            i++;
+                        }
+                    }
+
+                    if(sb.length() > 0) {
+                        stack.push(sb.toString());
+                        sb.setLength(0);
+                    }
+                }
+            }
+
+            if(!stack.isEmpty()) {
+                System.out.println("illegal");
+            }else {
+                System.out.println("legal");
+            }
+        }
+
+    }
+}
