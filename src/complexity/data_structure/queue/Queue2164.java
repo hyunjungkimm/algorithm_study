73f557638abdcccf99218124135b491ad2593c14
kimhyeonjeong <khjjung1124@gmail.com>
2024-06-09 19:56:44 +0900

    [Queue] 240609 - queue, dequeue

---
+++ src/complexity/data_structure/queue/Queue2164.java
@@ -0,0 +1,25 @@
+package src.complexity.data_structure.queue;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue2164 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while(queue.size() > 1) {
+            queue.remove();
+            queue.add(queue.remove());
+        }
+
+        System.out.println(queue.remove());
+    }
+}
